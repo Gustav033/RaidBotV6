5b463dea1166701b0eb6ccedc3ad979652b28cc9
Glitch (equable-gymnast) <none>
2019-10-12 16:48:06 +0000

    ✊🌇 Checkpoint ./commands/raid.js:6833819/105 ./commands/ping.js:6833819/173 ./commands/test.js:6833819/536

---
+++ commands/test.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js")
+
+exports.run = async (client, message) => {
+  let pingm = await message.channel.send("Calculando!")
+  let Ping = "Seu Ping:" + pingm.createdTimestamp - message.createdTimestamp + "ms | Ping Do Bot:" + Math.round(client.ping) + 'ms';
+  let embed = new discord.RichEmbed()
+  .setTitle("Test!")
+  .setDescription("Ping: " + Ping)
+  .addField("Bot Funcionando?", "Sim!")
+  .addField("Versão:", "V3")
+  .setColor("RANDOM")
+  .setFooter("Feito Por Gustavo33")
+}
