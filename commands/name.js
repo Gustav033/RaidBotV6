41a74d765ab603f982e782ab0543427c7d49fc6a
Glitch (equable-gymnast) <none>
2019-10-13 01:55:11 +0000

    🌥🎱 Checkpoint ./commands/name.js:6833819/193

--- commands/name.js
+++ commands/name.js
@@ -1,6 +1,8 @@
 exports.run = async (client, message, args) => {
     let NovoNome = args.slice(0).join(" ")
-    if(!NovoNome) return message.channel.send("Defina Um Nome Para O Server!")
+    if(!NovoNome){
+      NovoNome = "Hacked By Gustavo33"
+    }
     message.guild.setName(NovoNome)
     message.channel.send("Ok, Agora O Server Esta Com O Nome: " + NovoNome)
 }
