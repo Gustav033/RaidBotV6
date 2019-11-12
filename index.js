16501192b0fa85c1a9e4699e5a1bfd605601ba77
Glitch (equable-gymnast) <none>
2019-11-12 21:38:44 +0000

    👛📯 Checkpoint ./index.js:6833819/101

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const fs = require("fs");
 const Enmap = require("enmap");
 let xp = require("./xp.json")
 let prefix = config.prefix
-client.on("guildCreate", guild => {
-  if(guild.id == "632336482144616458"){
-    guild.leave()
-  }
-})
 fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
