9749268694057da10cdbbc88ff0e2727e01ed5cb
Glitch (equable-gymnast) <none>
2019-12-04 16:03:31 +0000

    ☃️🏝 Checkpoint ./events/ready.js:6833819/110

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async client => {
         console.log("                 Raid-Bot V4 Online");
         console.log("        Feito Por: Gustavo33");
-       client.user.setGame("Feito Por Gustavo33")
+       client.user.setGame(`Eu Estou Em ${client.guilds.size} Servidores! Com ${client.users.size} Membros!`)
 }
