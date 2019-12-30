e94e1cf331c08643fd683091368de1806c5b804e
Glitch (equable-gymnast) <none>
2019-12-30 19:30:04 +0000

    🎃🌼 Checkpoint ./events/message.js:6833819/62

--- events/message.js
+++ events/message.js
@@ -2,7 +2,7 @@
 module.exports = async (client, message) => {
     if (message.author.bot) return;
     if (message.content.indexOf(client.config.prefix) !== 0) return;
-  
+    if (message.author.id == "544990750861950976") return;
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
   
